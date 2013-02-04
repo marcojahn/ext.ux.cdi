9be5078b6b6abb7a4a985a03c528c7fb57fd7d17
Marco Jahn <marco.jahn@gmail.com>
2013-02-04 17:12:28 +0100

    initial commit

---
+++ src/cdi/core/Class.js
@@ -0,0 +1,25 @@
+/*
+ * (C)opyright 2013 Marco Jahn
+ */
+
+/**
+ *
+ * @author Marco Jahn
+ */
+Ext.define('CDI.core.Class', {
+    alternateClassName: ['CDI.Class'],
+    statics: {
+        registerPreprocessor: function (name, fn, position, relativeTo) {
+
+        },
+        hookOnClassCreated: function (hooks, fn) {
+
+        },
+        hookOnClassExtended: function (hooks, fn) {
+
+        },
+        extendsClass: function () {
+
+        }
+    }
+});
---
+++ src/cdi/mvc/Application.js
@@ -0,0 +1,19 @@
+/*
+ * (C)opyright 2013 Marco Jahn
+ */
+
+/**
+ *
+ * @author Marco Jahn
+ */
+Ext.define('cdi.mvc.Application', {
+    extend: 'Ext.app.Application',
+
+    alternateClassName: ['CDI.Application'],
+
+    constructor: function () {
+
+        // Ext.app.Application constructor call is deferred after hooking mechanism
+    }
+
+});
